0f0c57336a82eb1a7cce4919792412efc18821a9
Abiyamm <abiyamm91@gmail.com>
2023-10-05 13:11:28 -0700

    tasks

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-'''Task 1: Basic annotations - concat'''
+'''Task 1: Basic annotations - concat
+'''
+
 
 def concat(str1: str, str2: str) -> str:
-    '''Concatenates two strings. '''
+    '''Concatenates two strings.
+    '''
     return str1 + str2
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-'''Task 2: Basic annotations - floor'''
+'''Task 2: Basic annotations - floor
+'''
 
 def floor(a: float) -> int:
-    '''Computes the floor of a floating-point number.'''
+    '''Computes the floor of a floating-point number.
+    '''
     return int(a)
